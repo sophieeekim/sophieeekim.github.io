16e704db8c9dfa952837c1af89843f8825d7289f
sophie-kim <keun195@gmail.com>
2019-10-12 16:51:30 -0700

    navigation fixed  thumb added

--- scroll.js
+++ scroll.js
@@ -2,41 +2,60 @@
 const supportPageOffset = window.pageXOffset !== undefined;
 const isCSS1Compat = ((document.compatMode || "" ) === "CSS1Compat");
 const fadeItems = document.getElementsByClassName('fadeAnim');
-
+let scrollPos = 0;
 const scrollDown = document.getElementById('scrolldown');
+const topNav = document.querySelector('.nav-bar');
+const bottomNav = document.querySelector('.nav-article-bar');
 
 for(let item of fadeItems){
-	item.setAttribute('fade', 'false');
 	item.setAttribute('style','opacity: 0;');
 }
 
 window.addEventListener('scroll', event => {
 	"use strict";
 	const y = supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
+	toggleNav();
 
-	for(let item of fadeItems) {
-		if( (item.offsetTop - y) < 750){
-			toggleFadeIn(item);
+	if( fadeItems.length !== 0){
+		for(let item of fadeItems) {
+			if( (item.offsetTop - y) < 750){
+				toggleFadeIn(item);
+			} 
 		}
 	}
+
+	if(scrollDown !== null) {
 	toggleFadeOut(scrollDown);
+	}
+
 });
 
+function toggleNav(){
+	if ((document.body.getBoundingClientRect()).top > scrollPos) // scroll up
+	{
+		topNav.classList.remove('topNavDisappear');
+		if(bottomNav !== null){
+			bottomNav.classList.add('bottomNavDisappear');
+		}
+	}
+	else // scroll down
+	{	
+		topNav.classList.add('topNavDisappear');
+		if(bottomNav !== null){
+			bottomNav.classList.remove('bottomNavDisappear');
+		}
+	}
+	// saves the new position for iteration.
+	scrollPos = (document.body.getBoundingClientRect()).top;
+}
 
 function toggleFadeIn(target){
-	if(target.getAttribute('fade') === 'false') {
-		target.setAttribute('style','animation: fadeIn 1s ease-in;');
-		target.setAttribute('fade','true');
-	}
+		target.classList.add('fade-in');
+		target.classList.remove('fadeAnim');
 }
 
 function toggleFadeOut(target){
-	if(target.getAttribute('fade') === 'true') {
-		target.setAttribute('style','animation: fadeOut 1s ease-in; animation-fill-mode: forwards; -webkit-animation-fill-mode: forwards; -moz-animation-fill-mode: forwards; -o-animation-fill-mode: forwards;');
-		target.setAttribute('fade','false');
-	}
-
-	// target.setAttribute('style','opacity: 0;');
+		target.classList.add('fade-out');
 }
 
 function stickyFunc(target){
